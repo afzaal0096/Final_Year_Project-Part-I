7c56a1b0d58306d4fb975316ee43c3a40140c71f
Usman-Ayub <96381890+Usman-Ayub@users.noreply.github.com>
2024-05-17 17:09:25 +0500

    Add files via upload

---
+++ components/HeroSection/HeroSection.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useContext } from "react";
+import Image from "next/image";
+
+//INTERNAL IMPORT
+import Style from "./HeroSection.module.css";
+import { Button } from "../componentsindex";
+import images from "../../img";
+
+
+const HeroSection = () => {
+
+    return (
+    <div className={Style.heroSection}>
+      <div className={Style.heroSection_box}>
+        <div className={Style.heroSection_box_left}>
+          <h1>Create,Buy and Sell NFTS</h1>
+          <p>
+            Buy and Sell NFTS on our website.
+          </p>
+          <Button
+            btnName="Start your search"
+            />
+        </div>
+        <div className={Style.heroSection_box_right}>
+          <Image
+            src={images.hero}
+            alt="Hero section"
+            width={600}
+            height={600}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
